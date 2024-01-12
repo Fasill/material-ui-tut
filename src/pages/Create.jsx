1ebfa934dcf2606e4c321e71d16beb5e778a82b4
fasil <fasilhawultie19@gmail.com>
2024-01-12 16:47:25 +0300

    lesson2: Buttons

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 // import Typography from '@material-ui/core/Typography'
 import Typography from '@material-ui/core/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
 
 export default function Create() {
   return (
-    <Typography
-      variant = "h6"
-      color="textSecondary"
-      component="h2"
-      gutterBottom
-    >
-      Create a New Note
-    </Typography>
+    <Container>
+      <Typography
+        variant = "h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+      type='submit'
+      color='secondary'
+      variant='contained'
+      onClick= {()=>console.log("you clicked me")}
+      >
+        Submit
+      </Button>
+
+    </Container>
   )
 }
